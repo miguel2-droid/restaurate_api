f33d218d0498f4c4bd777c18909affca6c7efec0
Your Name <you@example.com>
2025-09-17 19:42:54 -0300

    ajustes

--- routers/clientes.py
+++ routers/clientes.py
@@ -7,16 +7,17 @@ import schemas
 
 router = APIRouter(prefix="/cliente", tags=["Clientes"])
 
+
 @router.post("/", response_model=schemas.Cliente)
 def criar(cliente: schemas.Cliente, db:Session = Depends(database.get_db)):
     return crud.criar_cliente(db, cliente)
 
-@router.get("/{listar}", response_model=schemas.Cliente)
+@router.get("/listar/", response_model=schemas.Cliente)
 def listar(cliente: schemas.Cliente, db:Session = Depends(database.get_db)):
     return crud.listar_cliente(db)
 
-@router.get("/{buscar}", response_class=schemas.Cliente)
-def buscar(cliente: schemas.Cliente, db:Session = Depends(database.get_db)):
+@router.get("/buscar")
+def buscar(nome: str, db:Session = Depends(database.get_db)):
     return crud.obter_cliente_por_nome(db, nome)
 
 @router.get("/{id}", response_model=schemas.Cliente)
